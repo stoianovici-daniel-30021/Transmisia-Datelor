eccc463c1678733751f998239938f2406915edc3
stoianovici-daniel-30021 <danielstoianovici218@gmail.com>
2021-04-03 15:54:44 +0300

    3.4.2021

--- lab-3/client/js/app.js
+++ lab-3/client/js/app.js
@@ -3,10 +3,10 @@ var app = new Vue({
   data: {
     dataBits: [],
     status: '',
-    numberOfDataBits: 4
+    numberOfDataBits: 8
   },
   created: function () {
-    this.initDataBits(4);
+    this.initDataBits(8);
   },
   methods: {
     initDataBits: function () {
@@ -23,36 +23,68 @@ var app = new Vue({
         // this.status = encodedMessage + ' encoded sent to server';
 
         return axios
-          .put('http://localhost:3000/message', { bits: encodedMessage })
-          .then(response => (this.status = response.data));
+            .put('http://localhost:3000/message', { bits: encodedMessage })
+            .then(response => (this.status = response.data));
       } else {
         this.status =
-          'Input is not valid. Please use 0 or 1 as data bit values';
+            'Input is not valid. Please use 0 or 1 as data bit values';
       }
     },
     encode: function (bits) {
-      // This function must be changed to allow any number of data bits
-      // Right now it only works for 4 data bits
-      console.log(this.numberOfDataBits);
-      var c4 = this.parity(
-        parseInt(bits[1].data) + parseInt(bits[2].data) + parseInt(bits[3].data)
-      );
-      var c2 = this.parity(
-        parseInt(bits[0].data) + parseInt(bits[2].data) + parseInt(bits[3].data)
-      );
-      var c1 = this.parity(
-        parseInt(bits[0].data) + parseInt(bits[1].data) + parseInt(bits[3].data)
-      );
-      console.log('Control bits: ' + c1 + ',' + c2 + ',' + c4);
-      return [
-        c1,
-        c2,
-        parseInt(bits[0].data),
-        c4,
-        parseInt(bits[1].data),
-        parseInt(bits[2].data),
-        parseInt(bits[3].data)
-      ];
+      var returnFunction = [];
+      if(this.numberOfDataBits==4){
+        console.log(this.numberOfDataBits);
+        var c4 = this.parity(
+            parseInt(bits[1].data) + parseInt(bits[2].data) + parseInt(bits[3].data)
+        );
+        var c2 = this.parity(
+            parseInt(bits[0].data) + parseInt(bits[2].data) + parseInt(bits[3].data)
+        );
+        var c1 = this.parity(
+            parseInt(bits[0].data) + parseInt(bits[1].data) + parseInt(bits[3].data)
+        );
+        console.log('Control bits: ' + c1 + ',' + c2 + ',' + c4);
+        returnFunction = [
+          c1,
+          c2,
+          parseInt(bits[0].data),
+          c4,
+          parseInt(bits[1].data),
+          parseInt(bits[2].data),
+          parseInt(bits[3].data)
+        ];
+      }
+      if(this.numberOfDataBits==8){
+        console.log(this.numberOfDataBits);
+        var c8 = this.parity(
+            parseInt(bits[4].data) + parseInt(bits[5].data) + parseInt(bits[6].data) + parseInt(bits[7].data)
+        );
+        var c4 = this.parity(
+            parseInt(bits[1].data) + parseInt(bits[2].data) + parseInt(bits[3].data) + parseInt(bits[7].data)
+        );
+        var c2 = this.parity(
+            parseInt(bits[0].data) + parseInt(bits[2].data) + parseInt(bits[3].data) + parseInt(bits[5].data) + parseInt(bits[6].data)
+        );
+        var c1 = this.parity(
+            parseInt(bits[0].data) + parseInt(bits[1].data) + parseInt(bits[3].data) + parseInt(bits[4].data) + parseInt(bits[6].data)
+        );
+        console.log('Control bits: ' + c1 + ',' + c2 + ',' + c4 + ',' + c8);
+        returnFunction = [
+          c1,
+          c2,
+          parseInt(bits[0].data),
+          c4,
+          parseInt(bits[1].data),
+          parseInt(bits[2].data),
+          parseInt(bits[3].data),
+          c8,
+          parseInt(bits[4].data),
+          parseInt(bits[5].data),
+          parseInt(bits[6].data),
+          parseInt(bits[7].data)
+        ];
+      }
+      return returnFunction;
     },
     parity: function (number) {
       return number % 2;
